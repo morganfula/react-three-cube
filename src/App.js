3b5a0184a89ea82ad067d9821cf1ec67b66d3573
morganfula <morgan.fula@me.com>
2020-09-24 08:58:11 +0200

    shadow added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.scss';
 
 import { Canvas, useFrame } from 'react-three-fiber';
 
+import { softShadows } from 'drei';
+
+softShadows()
+  ;
 const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh position={position} ref={mesh} >
+    <mesh castShadow position={position} ref={mesh} >
       <boxBufferGeometry attach='geometry' args={args} />
       <meshStandardMaterial attach='material' color={color} />
     </mesh >
@@ -18,13 +22,36 @@ const SpinningMesh = ({ position, args, color }) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+      <Canvas
+        shadowMap
+        colorManagement
+        camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
         <directionalLight
-          position
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
         />
         <pointLight position={[-10, 0, 20]} intensity={.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+        </group>
+
         <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='#a8ceef' />
         <SpinningMesh position={[-2, 1, -5]} color='#c9a2a5' />
         <SpinningMesh position={[5, 1, -2]} color='#c19869' />
